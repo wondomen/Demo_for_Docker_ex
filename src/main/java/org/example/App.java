99ac0135e81f45a4da7c1981d37de1b1b74173de
wondomen <berhanud@metropolia.fi>
2024-03-01 13:39:57 +0200

    intial commit/additional function added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+    public static int addMe(int a, int b) {
+        return a + b;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!  "  + addMe(200, 100));
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,14 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest{
+    App app = new App();
+    @Test
+    public void testAddMe() {
+        assertEquals(300, app.addMe(200, 100));
     }
 }
+
+
